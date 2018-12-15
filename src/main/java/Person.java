979456033a24668e125d35e768d580b15975ab3f
Bojan Antonovic <bojan@antonovic.ch>
2018-12-15 18:27:34 +0100

    Lombok

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,6 @@
+import lombok.Data;
+
+@Data
 public class Person {
 
     private final String secondName;
@@ -5,31 +8,4 @@ public class Person {
     private final String firstName;
 
     private final int age;
-
-    public Person(final String secondName, final String firstName, final int age) {
-        this.secondName = secondName;
-        this.firstName = firstName;
-        this.age = age;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "secondName='" + secondName + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
